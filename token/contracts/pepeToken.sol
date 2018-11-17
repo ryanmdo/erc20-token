9380f8247d85afce067c7fd08c8600fd1aba554c
ryanmdo <rdo9842@Gmail.com>
2018-11-17 14:10:43 -0800

    delgated transfers added, but test not complete

--- token/contracts/pepeToken.sol
+++ token/contracts/pepeToken.sol
@@ -9,6 +9,11 @@ contract pepeToken{
     uint256 public totalSupply;
     mapping(address=>uint256) public balanceOf; 
 
+    mapping(address=>mapping(address=>uint256)) public allowed;
+
+    //this is an array of addresses with itss own address-value key-value pait
+    //those values are how much the first address is allowing the second address to spend
+
     //address and uint256 are key:value pairs
     //they are like functions
     //balanceOF is a public variable of the contract
@@ -25,19 +30,61 @@ contract pepeToken{
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
 
     //to, value, and from, it also must be publicly accessible and return a status of success
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value,"You have not enough funds.");
-
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
-
-        Transfer(msg.sender, _to, _value);
-
-        //What is emitting? IT says that the contraxct requires it
+        emit Transfer(msg.sender, _to, _value);
         return true; //only calls if the require was success first
     }
 
+    /*  Add the ability for delgated transfer
+    *   This is for when a non-token owner is calling the contract
+    *   
+    *
+    *   event Approval
+    *   approve
+    *   allowance
+    *   transferFrom
+    */
 
+
+    function approve(address _spender,uint256 _value)public returns (bool success) {
+        //apprently, lots of functions on ethereum need the returns
+        //_spender is the one who is approved to spend the value on his behalf
+        
+        allowed[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender,_spender,_value);
+        return true;
+
+
+        
+    }
+
+
+
+    function allowance(address _owner, address _spender) public view returns (uint remaining) {
+        
+        return allowed[_owner][_spender];
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public view returns (bool success) {
+
+
+        require(_value <= balanceOf[_from],"require that tokens exist");
+        require(_value <= allowed[_from][msg.sender],"require proper approval for spending tokens");
+        //change balance, update allowed, allowance big enough
+        //call transfer event
+    }
+    
 }
--- token/test/pepeToken.js
+++ token/test/pepeToken.js
@@ -34,13 +34,14 @@ contract('pepeToken', function(accounts) { //we are graabbing the pepToken someh
 
 
 
-    it('transfers token owndership', function(){
+    it('transfers token ownership', function(){
         return pepeToken.deployed().then(function(instance) {
             tokenInstance = instance;
             return tokenInstance.transfer.call(accounts[0],999999999999999999999);
         }).then(assert.fail).catch(function(error){
             assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
             return tokenInstance.transfer.call(accounts[1], 900, {from: accounts[0] });
+            // the .call after tranfer doesn't actually make the transfer happen, but it gets the returned value
         }).then(function(success) {
             assert.equal(success, true, "it returns true");
             return tokenInstance.transfer(accounts[1], 900, {from: accounts[0] });
@@ -63,5 +64,54 @@ contract('pepeToken', function(accounts) { //we are graabbing the pepToken someh
     });
 
 
+
+
+    it('approves token transfer', function(){
+        return pepeToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success){
+            assert.equal(success, true, 'check for proper approval')
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0]});
+        }).then(function(receipt){
+            // console.log(receipt.logs)
+            assert.equal(receipt.logs.length,1,'');
+            assert.equal(receipt.logs[0].event,'Approval','assert proper event "Approval"');
+            assert.equal(receipt.logs[0].args._owner,accounts[0],'assert proper logging of address of owner');
+            assert.equal(receipt.logs[0].args._spender,accounts[1],'assert proper logging of address of spender');
+            assert.equal(receipt.logs[0].args._value,100,'assert proper logging of value');
+            // return tokenInstance.allowance(accounts[0],accounts[1]);
+        })
+    
+    
+    
+    });
+
+
+
+
+    it('handles delegated token transfers', function() {
+        return pepeToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = account[3];
+            spendingAccount= account[4];
+            return tokenInstance.transfer(fromAccount, 100, {fromt: accounts[0]});
+        }).then(function(receipt){
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount})
+        }).then(function(receipt){
+            return tokenInstance.transferFrom(fromAccount, toAccount, 99999999, {from: spendingAccount})
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'assert that it cannot transfer larger than balance');
+            return tokenInstance.transferFrom(fromAccount,toAccount,50,{from: spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            console.log(error)
+            assert(error.message.indexOf('revert') >= 0, 'assert that it cannot transfer larger than approved amount');
+            return tokenInstance.transferFrom.call(fromAccount,toAccount, 10, {from: spendingAccount});
+        }).then(function(success){
+            console.log(success)
+        });
+    
+    });
     
 });
